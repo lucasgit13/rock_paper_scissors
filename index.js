a978da1f20e101dfc5201d11bbfe8532139c0a1a
lucasgit13 <lucas.araujo.souza13@gmail.com>
2022-05-15 18:42:40 -0300

    add improvements

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-function computerPlay() {
-  const playsOptions = ["Rock", "Paper", "Scissors"];
+const PLAY_OPTIONS = ["Rock", "Paper", "Scissors"];
+
+function computerPlay(playsOptions) {
   return playsOptions[Math.floor(Math.random() * playsOptions.length)];
 }
 
 function playerSelection() {
-  const choice = prompt("type your play: \n1. Rock\n2. Paper\n3. Scissors");
+  const choice = prompt(
+    "type your play: \n1. Rock\n2. Paper\n3. Scissors\n4. Sair"
+  );
   switch (choice) {
     case "1":
       return "Rock";
@@ -12,6 +15,8 @@ function playerSelection() {
       return "Paper";
     case "3":
       return "Scissors";
+    case "4":
+      return "break";
     case null:
       return "Player didn't choose";
     default:
@@ -20,65 +25,71 @@ function playerSelection() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  let result = "It's a tie!";
-  let whoWon = "tie";
+  if (playerSelection == "Player didn't choose")
+    return {
+      whoWon: "nobody",
+      message: "Invalid input! Game aborted...",
+    };
+
   playerSelection = playerSelection.toLowerCase();
   computerSelection = computerSelection.toLowerCase();
-  if (playerSelection == computerSelection) return [result, whoWon];
 
-  switch (playerSelection) {
-    case "rock":
-      if (computerSelection == "paper") {
-        result = "You lost! Paper beats Rock";
-        whoWon = "computer";
-      } else {
-        whoWon = "player";
-        result = "You win! Rock beats Scissors";
-      }
-      break;
-    case "paper":
-      if (computerSelection == "scissors") {
-        result = "You lost! Scissors beats Paper";
-        whoWon = "computer";
-      } else {
-        whoWon = "player";
-        result = "You win! Paper beats Rock";
-      }
+  if (playerSelection == computerSelection)
+    return {
+      whoWon: "tie",
+      message: "It's a tie!",
+    };
 
-      break;
-    case "scissors":
-      if (computerSelection == "rock") {
-        result = "You lost! Rock beats Scissors";
-        whoWon = "player";
-      } else {
-        whoWon = "player";
-        result = "You win! Scissors beats Paper";
-      }
-      break;
-    default:
-      whoWon = "nobody";
-      result = "Game Aborted!";
-  }
-  return [result, whoWon];
+  if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper")
+  )
+    return {
+      whoWon: "player",
+      message: `You win! ${playerSelection} beats ${computerSelection}!`,
+    };
+  else
+    return {
+      whoWon: "computer",
+      message: `You lost! ${computerSelection} beats ${playerSelection}!`,
+    };
 }
 
-function game(playRound) {
+function game(playRound, timesPlay) {
+  if (!confirm("Let's play?")) return;
+
+  let count = timesPlay;
   let userCount = 0;
   let computerCount = 0;
 
-  for (let i = 1; i <= 5; i++) {
-    const player = playerSelection();
-    const computer = computerPlay();
-    const [result, whoWon] = playRound(player, computer);
-    if (whoWon == "player") userCount += 1;
-    if (whoWon == "computer") computerCount += 1;
-    console.log(`[${i}/5]`);
+  while (count > 0) {
+    const player = playerSelection(PLAY_OPTIONS) || "Invalid play";
+    if (player == "break") break;
+    const computer = computerPlay(PLAY_OPTIONS);
+    const play = playRound(player, computer);
+    const message = play.message;
+    const whoWon = play.whoWon;
+
+    if (whoWon == "player") {
+      userCount += 1;
+      count--;
+    }
+    if (whoWon == "computer") {
+      computerCount += 1;
+      count--;
+    }
+    if (whoWon == "tie") {
+      count--;
+    }
+    console.log(`[${count}/${timesPlay}]`);
     console.log(`Player: ${player}\nComputer: ${computer}`);
-    console.log(result);
+    console.log(message);
   }
   if (userCount > computerCount) console.log("You won the round!");
   if (userCount < computerCount) console.log("You lost the round!");
+  if (userCount == computerCount) console.log("It's a tie!");
   console.log(`Score:\n\tPlayer: ${userCount}\n\tComputer: ${computerCount}`);
 }
 
-game(playRound);
+game(playRound, 5);
