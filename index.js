2670cae861a78466319e2a590fbee3f641d874d0
lucasgit13 <lucas.araujo.souza13@gmail.com>
2022-05-14 21:40:16 -0300

    Add playRound function

--- index.js
+++ index.js
@@ -3,4 +3,30 @@ function computerPlay() {
   return playsOptions[Math.floor(Math.random() * playsOptions.length)];
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+  let result = "It's a tie!";
+  if (playerSelection == computerSelection) return result;
+
+  switch (playerSelection) {
+    case "Rock":
+      computerSelection == "Paper"
+        ? (result = "You lost! Paper beats Rock")
+        : (result = "You win! Rock beats Scissors");
+      break;
+
+    case "Paper":
+      computerSelection == "Rock"
+        ? (result = "You win! Paper beats Rock")
+        : (result = "You lost! Scissors beats Paper");
+      break;
+
+    case "Scissors":
+      computerSelection == "Paper"
+        ? (result = "You win! Scissors beats Paper")
+        : (result = "You lost! Rock beats Scissors");
+      break;
+  }
+  return result;
+}
+
+console.log(playRound("Paper", computerPlay()));
